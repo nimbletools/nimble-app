b795281494b70923114d66e6220c9d765e421365
Codecat <spansjh@gmail.com>
2017-07-04 21:41:29 +0200

    Implement ini parser

---
+++ include/nimble/ini.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "common.h"
+
+namespace na
+{
+	class Ini;
+
+	class IniPair
+	{
+	friend class Ini;
+
+	public:
+		s2::string Key;
+
+	private:
+		s2::string m_value;
+
+	public:
+		int AsInt();
+		float AsFloat();
+		bool AsBool();
+		s2::string AsString();
+		glm::ivec2 AsIvec2();
+		glm::vec2 AsVec2();
+		glm::vec3 AsVec3();
+		glm::vec4 AsVec4();
+		glm::vec4 AsColor();
+	};
+
+	class IniSection
+	{
+	public:
+		s2::string Name;
+		s2::list<IniPair> Pairs;
+
+	public:
+		IniPair* Get(const char* key);
+	};
+
+	class Ini : private IniSection
+	{
+	public:
+		s2::list<IniSection> Sections;
+
+	public:
+		Ini(const char* filename);
+
+		IniSection* GetSection(const char*  name);
+
+		IniPair* Get(const char* section, const char* key);
+
+	private:
+		void Load(const char* filename);
+	};
+}
---
+++ src/nimble/ini.cpp
@@ -0,0 +1,94 @@
+#include <nimble/common.h>
+#include <nimble/ini.h>
+
+#include <nimble/utils.h>
+
+int na::IniPair::AsInt() { return atoi(m_value); }
+float na::IniPair::AsFloat() { return atof(m_value); }
+bool na::IniPair::AsBool() { return m_value == "true"; }
+s2::string na::IniPair::AsString() { return m_value; }
+glm::ivec2 na::IniPair::AsIvec2() { return ParseIvec2(m_value); }
+glm::vec2 na::IniPair::AsVec2() { return ParseVec2(m_value); }
+glm::vec3 na::IniPair::AsVec3() { return ParseVec3(m_value); }
+glm::vec4 na::IniPair::AsVec4() { return ParseVec4(m_value); }
+glm::vec4 na::IniPair::AsColor() { return ParseColor(m_value); }
+
+na::IniPair* na::IniSection::Get(const char* key)
+{
+	for (IniPair &pair : Pairs) {
+		if (pair.Key == key) {
+			return &pair;
+		}
+	}
+	return nullptr;
+}
+
+na::Ini::Ini(const char* filename)
+{
+	Name = filename;
+	Load(filename);
+}
+
+na::IniSection* na::Ini::GetSection(const char* name)
+{
+	for (IniSection &section : Sections) {
+		if (section.Name == name) {
+			return &section;
+		}
+	}
+	return nullptr;
+}
+
+na::IniPair* na::Ini::Get(const char* section, const char* key)
+{
+	IniSection* s = GetSection(section);
+	if (s == nullptr) {
+		return nullptr;
+	}
+	return s->Get(key);
+}
+
+void na::Ini::Load(const char* filename)
+{
+	FILE* fh = fopen(filename, "rb");
+
+	IniSection* currentSection = (IniSection*)this;
+
+	char line[512];
+	while (fgets(line, 512, fh) != nullptr) {
+		if (*line == ';' || *line == '#') {
+			continue;
+		}
+
+		char* endl = strchr(line, '\r');
+		if (endl == nullptr) {
+			endl = strchr(line, '\n');
+		}
+		if (endl != nullptr) {
+			*endl = '\0';
+		}
+
+		if (endl == line) {
+			continue;
+		}
+
+		if (*line == '[') {
+			currentSection = &Sections.add();
+			currentSection->Name = s2::string(line).trim(" []");
+			printf("Section: \"%s\"\n", (const char*)currentSection->Name);
+			continue;
+		}
+
+		s2::stringsplit parse(line, "=", 2);
+		if (parse.len() != 2) {
+			printf("Invalid line: \"%s\"\n", line);
+			continue;
+		}
+
+		IniPair &pair = currentSection->Pairs.add();
+		pair.Key = parse[0].trim();
+		pair.m_value = parse[1].trim();
+	}
+
+	fclose(fh);
+}
